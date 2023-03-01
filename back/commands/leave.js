0cbb08d8da9c386c39320847a556bb0adee37d45
Guillaume riedinger <guillaume.riedinger@weibis-nord.fr>
2023-03-01 23:03:53 +0100

    Factorising vocal methods

--- back/commands/leave.js
+++ back/commands/leave.js
@@ -1,15 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
-const voiceData = require('../voice-data.js');
+const soundManager = require('../sound-manager.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('leave')
 		.setDescription('Oh ok ... I will leave the channel ...'),
 	async execute(interaction) {
-		let connection = voiceData.connections[interaction.guildId];
-		if (connection) {
-			connection.destroy();
-			voiceData.connections[interaction.guildId] = undefined;
-		}
+		soundManager.leaveChannel(interaction.guild);
+
+		interaction.reply("Très bien, je m'en vais :)");
 	},
 };
--- back/commands/swing.js
+++ back/commands/swing.js
@@ -1,7 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
-const path = require('node:path');
-const voiceData = require('../voice-data.js');
+const soundManager = require('../sound-manager.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,24 +7,12 @@ module.exports = {
 		.setDescription('I will play some nice music ;) !'),
 	async execute(interaction) {
 		console.log("Current guildId is " + interaction.guildId + ", channelId is " + interaction.member.voice.channelId);
+		if (!interaction.member.voice.channelId){
+            await interaction.reply('User is not in a channel');
+			return;
+        }
 
-		let connection = joinVoiceChannel({
-			channelId: interaction.member.voice.channelId,
-			guildId: interaction.guildId,
-			adapterCreator: interaction.guild.voiceAdapterCreator
-		});
-
-		voiceData.connections[interaction.guildId] = connection;
-
-
-		const player = createAudioPlayer();
-
-		let audioPath = path.join(__dirname, 'sounds/audio.mp3');
-		const resource = createAudioResource(audioPath, { inlineVolume: true });
-		resource.volume.setVolume(0.5);
-
-		player.play(resource);
-		connection.subscribe(player);
+		soundManager.playSound(interaction.guild, interaction.member.voice.channelId, 'audio');
 
 		await interaction.reply('Ca part en musique ;)');
 	},
