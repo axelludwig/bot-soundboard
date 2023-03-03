6c61c531ec6891d09e64553623a9e968d716b475
Guillaume riedinger <guillaume.riedinger@weibis-nord.fr>
2023-03-03 19:21:27 +0100

    Adding check

--- back/modules/discord/event-manager.js
+++ back/modules/discord/event-manager.js
@@ -12,6 +12,10 @@ function onUserChangeChannel(voiceState) {
     if (voiceState.guild.id == discordConfig.guildId && voiceState.id == discordConfig.clientId) {
         //Le bot a changé de channel
         channelManager.exportCurrentChannel().then((value) => {
+            if (!value){
+                return;
+            }
+
             console.log('send bot change channel : ' + value.id);
             socket.io.emit('botChangeChannel', value.id);
         });
