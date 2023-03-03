2ef145ff49b63e68c9c406592107be8709d5b948
Guillaume riedinger <guillaume.riedinger@weibis-nord.fr>
2023-03-03 18:01:45 +0100

    Adding guild caching

--- back/modules/discord/guild-manager.js
+++ back/modules/discord/guild-manager.js
@@ -1,9 +1,19 @@
 const discordClientManager = require("./discord-client");
 const discordConfig = require("../../discord-config.json");
 
-exports.getCurrentGuild = async function () {
-    let guilds = await discordClientManager.client.guilds.fetch();
-    let myGuild = await guilds.get(discordConfig.guildId).fetch();
+let cachedGuild = undefined;
 
-    return myGuild;
+exports.getCurrentGuild = async function () {
+    if (!cachedGuild){
+        let guilds = await discordClientManager.client.guilds.fetch();
+        let myGuild = await guilds.get(discordConfig.guildId).fetch();
+
+        cachedGuild = myGuild;
+        return myGuild;
+    }
+    else{
+        console.log("Guild found in cache");
+        return cachedGuild;
+
+    }
 }
